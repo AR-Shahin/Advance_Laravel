8edcf0dd8025835518f48b39702525d47cd5bca6
AR-Shahin <mdshahinmije96@gmail.com>
2021-02-04 23:19:30 +0600

    add queue

--- app/Listeners/DoctorCreatedListener.php
+++ app/Listeners/DoctorCreatedListener.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class DoctorCreatedListener
+class DoctorCreatedListener implements ShouldQueue
 {
     public $doctor;
     /**
